eaff064b45362881033dc9abe71a8cddfeea59a2
Parviz_jr <lenova@kali.kali>
2025-01-27 19:19:55 +0500

    if debug ketdi choco apdagi

--- choco/urls.py
+++ choco/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
 ]
 
 # Serve media files during development (only necessary in DEBUG mode)
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
