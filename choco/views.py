937d0d08b7315b12d291e945a651b86f4252559c
Parviz_jr <lenova@kali.kali>
2025-01-27 19:03:24 +0500

    404 hendler ketdi

--- choco/views.py
+++ choco/views.py
@@ -152,12 +152,3 @@ def custom_logout(request):
 
 
 
-
-from django.http import HttpResponseNotFound
-from django.template.loader import render_to_string
-from django.conf import settings
-
-def custom_404_view(request, exception):
-    response = render_to_string('404.html', {})
-    return HttpResponseNotFound(response)
-
--- core/urls.py
+++ core/urls.py
@@ -14,14 +14,11 @@ urlpatterns = [
 ]
 
 
-handler404 = 'choco.views.custom_404_view'
+
 
  
 
 #Bu kod bloklari Django’da DEBUG rejimi faolligida statik va media fayllarni xizmat ko'rsatish uchun kerak. Keling, har birini tushuntirib beram
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
-if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
