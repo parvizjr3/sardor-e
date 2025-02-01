bd513daa14c100d55286b8fd434c09651e061353
Parviz_jr <lenova@kali.kali>
2025-02-01 23:17:08 +0500

    hozir esa false ketdi

--- core/settings.py
+++ core/settings.py
@@ -14,7 +14,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')  
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['shoco-pie-samarkand.onrender.com', 'localhost', '127.0.0.1']
 
